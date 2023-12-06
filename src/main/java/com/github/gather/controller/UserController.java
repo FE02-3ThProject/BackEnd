1a58da25bc70da87dd95cbf8a1246f8b79e7ff8a
ZUA <gimsojeong0228@gmail.com>
2023-12-06 14:56:48 +0900

    UserSearch

--- src/main/java/com/github/gather/controller/UserController.java
+++ src/main/java/com/github/gather/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.github.gather.controller;
 
+import com.github.gather.dto.UserDTO;
 import com.github.gather.dto.request.UserLoginRequest;
 import com.github.gather.dto.request.UserSignupRequest;
 import com.github.gather.dto.response.UserLoginResponse;
@@ -7,8 +8,10 @@ import com.github.gather.entity.User;
 import com.github.gather.repositroy.UserRepository;
 import com.github.gather.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
 
 import javax.transaction.Transactional;
@@ -25,16 +28,15 @@ public class UserController {
 
     @Transactional
     @PostMapping(value = "/signup")
-    public ResponseEntity<?> userSignup(@RequestBody UserSignupRequest user){
+    public ResponseEntity<?> userSignup(@RequestBody UserSignupRequest user) {
         User newUser = userService.signup(user);
         userRepository.save(newUser);
         return ResponseEntity.status(200).body(newUser);
     }
 
 
-
     @PostMapping("/login")
-    public ResponseEntity<?> userLogin(@RequestBody UserLoginRequest user){
+    public ResponseEntity<?> userLogin(@RequestBody UserLoginRequest user) {
         UserLoginResponse loginUser = userService.login(user);
 
         Map<String, String> response = new HashMap<>();
@@ -52,6 +54,19 @@ public class UserController {
     }
 
 
-    @GetMapping
-    public ResponseEntity<>
+    @GetMapping("/info")
+    public ResponseEntity<UserDTO> getUserInfo(@RequestParam String email) {
+        // email 값을 사용하여 정보를 조회 로직을 수행
+        // 이 부분에서 userService를 이용하여 실제 로직을 수행할 것
+        UserDTO userinfo = userService.getUserInfoByEmail(email);
+
+        if (userinfo != null) {
+            // 정상적으로 사용자 정보를 가져왔을 경우 200으로 OK응답을 반환
+            return ResponseEntity.ok(userinfo);
+        } else {
+            // 사용자 정보를 찾을 수 없는 경우 404 Not Found 응답을 반환
+            return ResponseEntity.notFound().build();
+        }
+
+    }
 }
--- src/main/java/com/github/gather/dto/UserDTO.java
+++ src/main/java/com/github/gather/dto/UserDTO.java
@@ -4,4 +4,19 @@ public class UserDTO {
     private String nickname;
     private String email;
 
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
