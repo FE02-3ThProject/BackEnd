056b23b0236887ce51103a01be7adea997a6b2e2
ChaNuks <cksdnrtls33@gmail.com>
2023-12-16 05:06:13 +0900

    Add : 모임방 생성과 동시에 chat_room 생성

--- src/main/java/com/github/gather/repositroy/group/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.github.gather.repositroy.group;
-
-import com.github.gather.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category,Long> {
-}
--- src/main/java/com/github/gather/repositroy/group/LocationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.github.gather.repositroy.group;
-
-import com.github.gather.entity.Location;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LocationRepository extends JpaRepository<Location, Long> {
-}
