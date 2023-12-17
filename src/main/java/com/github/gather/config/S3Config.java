2e4810686e56dfdd1ba26ca4a7f866884141b63d
ChaNuks <cksdnrtls33@gmail.com>
2023-12-18 01:48:43 +0900

    Modify : 모임방 이미지 수정

--- src/main/java/com/github/gather/config/S3Config.java
+++ src/main/java/com/github/gather/config/S3Config.java
@@ -10,15 +10,14 @@ import software.amazon.awssdk.services.s3.S3Client;
 @Configuration
 public class S3Config {
 
-    @org.springframework.beans.factory.annotation.Value("AKIA2WSJ73WOI2ZIWIMW")
+    @org.springframework.beans.factory.annotation.Value("accessKeyId")
     private String accessKeyId;
 
-    @org.springframework.beans.factory.annotation.Value("EIGmAvkGbdSKIddN/cuzWovuIM2t79kbwD4AACKN")
+    @org.springframework.beans.factory.annotation.Value("secretKey")
     private String secretKey;
 
-    @org.springframework.beans.factory.annotation.Value("ap-northeast-2")
+    @org.springframework.beans.factory.annotation.Value("region")
     private String region;
-
 
     @Bean
     public S3Client s3Client() {
