93a3b60af09ff71a9a6476856b29f72ad3f3ed9d
ZUA <gimsojeong0228@gmail.com>
2023-12-18 09:25:14 +0900

    aws upload

--- src/test/java/com/github/gather/GatherApplicationTests.java
+++ src/test/java/com/github/gather/GatherApplicationTests.java
@@ -1,13 +1,13 @@
-package com.github.gather;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class GatherApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.github.gather;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class GatherApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
