6d890d76d38e8ff4989890a0516d84daaea45094
ChaNuks <cksdnrtls33@gmail.com>
2023-12-18 11:48:51 +0900

    Add : 모임방 수정(이미지 업로드)

--- src/main/java/com/github/gather/controller/ImageController.java
+++ src/main/java/com/github/gather/controller/ImageController.java
@@ -1,6 +1,7 @@
 package com.github.gather.controller;
 
 import com.github.gather.dto.request.group.CreateGroupRequest;
+import com.github.gather.dto.request.group.UpdateGroupInfoRequest;
 import com.github.gather.service.S3Service;
 import com.github.gather.service.group.GroupService;
 import com.sun.security.auth.UserPrincipal;
@@ -8,20 +9,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.time.LocalDate;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/image")
+@RequestMapping("/api/group")
 public class ImageController {
 
     private final GroupService groupService;
@@ -32,6 +29,7 @@ public class ImageController {
         this.s3Service = s3Service;
     }
 
+    // 모임방 생성
     @PostMapping
     public ResponseEntity<?> createGroup(
             @RequestParam("file") MultipartFile file,
@@ -50,7 +48,7 @@ public class ImageController {
                 fout.write(file.getBytes());
             }
 
-            // S3에 그룹 생성과 파일 업로드
+            // S3에 파일 업로드
             String imageUrl = s3Service.uploadFile(convertFile.getName(), convertFile);
 
             CreateGroupRequest newGroupRequest = CreateGroupRequest.builder()
@@ -67,12 +65,57 @@ public class ImageController {
 
         } catch (Exception e) {
             // 에러 처리
-            log.error("그룹 생성 중 오류 발생", e);  // 에러 로그 출력
+            log.error("그룹 생성 중 오류 발생", e);
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("그룹 생성 중 오류 발생: " + e.getMessage());
         }
 
         // 업로드 성공 메시지 반환
         return ResponseEntity.ok("업로드 성공");
+    }
+
+    // 모임방 수정
+    @PutMapping("/{groupId}")
+    public ResponseEntity<?> updateGroupImage(
+            @PathVariable("groupId") Long groupId,
+            @RequestParam("file") MultipartFile file,
+            @RequestParam("name") String title,
+            @RequestParam("description") String description,
+            @RequestParam("maxMembers") Integer maxMembers,
+            @RequestParam("categoryId") Long categoryId,
+            @RequestParam("locationId") Long locationId,
+            @AuthenticationPrincipal UserPrincipal userPrincipal
+    ) {
+        try {
+            // MultipartFile을 java.io.File로 변환
+            File convertFile = new File(file.getOriginalFilename());
+            convertFile.createNewFile();
+            try (FileOutputStream fout = new FileOutputStream(convertFile)) {
+                fout.write(file.getBytes());
+            }
+
+            // S3에 이미지 업로드
+            String imageUrl = s3Service.uploadFile(convertFile.getName(), convertFile);
+
+            // 모임 정보 수정
+            UpdateGroupInfoRequest updateGroupInfo = UpdateGroupInfoRequest.builder()
+                    .title(title)
+                    .image(imageUrl)
+                    .description(description)
+                    .maxMembers(maxMembers)
+                    .categoryId(categoryId)
+                    .locationId(locationId)
+                    .build();
+
+            groupService.modifyGroupInfo(userPrincipal.getName(), groupId, updateGroupInfo);
+
+        } catch (Exception e) {
+            // 에러 처리
+            log.error("모임방 이미지 수정 중 오류 발생", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("모임방 이미지 수정 중 오류 발생: " + e.getMessage());
+        }
+
+        // 업로드 성공 메시지 반환
+        return ResponseEntity.ok("이미지 업로드 성공");
     }
 }
 
