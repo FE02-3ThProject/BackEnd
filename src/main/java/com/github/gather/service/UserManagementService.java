2f96104428b247aa249934c6d7e97629bbf66268
jin1220 <h6779@naver.com>
2023-12-20 00:42:18 +0900

    userinfo 수정 완료

--- src/main/java/com/github/gather/service/UserManagementService.java
+++ src/main/java/com/github/gather/service/UserManagementService.java
@@ -22,7 +22,7 @@ public class UserManagementService {
                 .orElseThrow(() -> new UserNotFoundException("회원 정보를 찾을 수 없습니다."));
 
         // 필요한 정보를 UserInfoResponse 객체를 생성하여 반환
-        return new UserInfoResponse(user.getUserId(), user.getNickname(), user.getEmail(),  user.getImage(), user.getLocationId(),user.getCategoryId());
+        return new UserInfoResponse(user.getUserId(), user.getNickname(), user.getEmail(),  user.getImage(), user.getLocationId(),user.getCategoryId(),user.getIntroduction());
     }
 
     public UserEditResponse editUserInfo(Long userId, UserEditRequest userEditRequest) {
