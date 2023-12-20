f54b2c1e9dbd19b46044278a8aee398e564c175b
ceciiirang <ceciiirang@gmail.com>
2023-12-20 12:09:54 +0900

    add: 로그인없이 전체 모임 조회, 카테고리별 모임 조회, 지역별 모임 조회, 제목별 모임 조회 수정.

--- src/main/java/com/github/gather/controller/group/GroupController.java
+++ src/main/java/com/github/gather/controller/group/GroupController.java
@@ -3,7 +3,6 @@ package com.github.gather.controller.group;
 import com.github.gather.dto.request.group.CreateGroupRequest;
 import com.github.gather.dto.request.group.UpdateGroupInfoRequest;
 import com.github.gather.dto.response.group.*;
-import com.github.gather.exception.UserRuntimeException;
 import com.github.gather.exception.group.GroupNotFoundException;
 import com.github.gather.security.JwtTokenProvider;
 import com.github.gather.service.group.GroupServiceImpl;
@@ -26,7 +25,7 @@ public class GroupController {
     public ResponseEntity<?> createGroup(@RequestBody CreateGroupRequest newGroupRequest, HttpServletRequest request) {
         String userToken = jwtTokenProvider.resolveToken(request);
         String userEmail = jwtTokenProvider.getUserEmail(userToken); //유저이메일인지 확인
-        return ResponseEntity.status(200).body(groupService.createGroup(userEmail,newGroupRequest));
+        return ResponseEntity.status(200).body(groupService.createGroup(userEmail, newGroupRequest));
     }
 
     //모임수정 -- 방장권한
@@ -34,89 +33,64 @@ public class GroupController {
     public ResponseEntity<?> modifyGroupInfo(@PathVariable Long groupId, @RequestBody UpdateGroupInfoRequest updateGroupInfoRequest, HttpServletRequest request) {
         String userToken = jwtTokenProvider.resolveToken(request);
         String userEmail = jwtTokenProvider.getUserEmail(userToken); //유저이메일인지 확인
-        return ResponseEntity.status(200).body(groupService.modifyGroupInfo(userEmail,groupId,updateGroupInfoRequest));
+        return ResponseEntity.status(200).body(groupService.modifyGroupInfo(userEmail, groupId, updateGroupInfoRequest));
     }
 
     //모임 삭제 --방장권한
     @DeleteMapping(value = "/delete/{groupId}")
-    public ResponseEntity<?> deleteGroup(@PathVariable Long groupId,  HttpServletRequest request) {
+    public ResponseEntity<?> deleteGroup(@PathVariable Long groupId, HttpServletRequest request) {
         String userToken = jwtTokenProvider.resolveToken(request);
         String userEmail = jwtTokenProvider.getUserEmail(userToken);
-        String result = groupService.deleteGroup(userEmail,groupId);
+        String result = groupService.deleteGroup(userEmail, groupId);
         return ResponseEntity.status(200).body(result);
     }
 
     //모임 전체 조회
     @GetMapping(value = "/all")
     public ResponseEntity<?> searchAllGroups(HttpServletRequest request) {
-        String userToken = jwtTokenProvider.resolveToken(request);
-        if(userToken != null) {
-            List<GroupListResponse> allGroups = groupService.searchAllGroups();
-            return ResponseEntity.status(200).body(allGroups);
-        }else {
-            throw new UserRuntimeException("모임 카페고리를 조회하려면 로그인이 필요합니다.");
-        }
+        List<GroupListResponse> allGroups = groupService.searchAllGroups();
+        return ResponseEntity.status(200).body(allGroups);
     }
 
-    //모임 상세 조회
+    //모임 상세 조회 --없는 groupId면 조회 예외처리
     @GetMapping(value = "/detail/{groupId}")
     public ResponseEntity<?> getGroupDetail(@PathVariable Long groupId, HttpServletRequest request) {
-        String userToken = jwtTokenProvider.resolveToken(request);
-        if(userToken != null) {
-            GroupDetailResponse groupDetail = groupService.getGroupDetail(groupId);
-            return ResponseEntity.status(200).body(groupDetail);
-        }else {
-            throw new GroupNotFoundException();
-        }
+        GroupDetailResponse groupDetail = groupService.getGroupDetail(groupId);
+        return ResponseEntity.status(200).body(groupDetail);
     }
 
     //모임 멤버 조회 -- 모임에 가입한 유저만?
     @GetMapping(value = "/groupMembers/{groupId}")
     public ResponseEntity<?> findGroupMemebers(@PathVariable Long groupId, HttpServletRequest request) {
         String userToken = jwtTokenProvider.resolveToken(request);
-        if(userToken != null) {
+        if (userToken != null) {
             List<GroupMemberListResponse> groupMembers = groupService.findGroupMembers(groupId);
             return ResponseEntity.status(200).body(groupMembers);
-        }else {
+        } else {
             throw new GroupNotFoundException();
         }
     }
 
-    //카테고리별 모임 조회 --로그인필수
+    //카테고리별 모임 조회 --예외처리 필요
     @GetMapping(value = "/category/{categoryId}")
-    public ResponseEntity<?> searchGroupsByCategoryId(@PathVariable Long categoryId,  HttpServletRequest request) {
-        String userToken = jwtTokenProvider.resolveToken(request);
-        if(userToken != null) {
-            List<GroupListByCategoryResponse> groupListByCategoryId = groupService.searchGroupsByCategoryId(categoryId);
-            return ResponseEntity.status(200).body(groupListByCategoryId);
-        }else {
-            throw new UserRuntimeException("모임 카페고리를 조회하려면 로그인이 필요합니다.");
-        }
+    public ResponseEntity<?> searchGroupsByCategoryId(@PathVariable Long categoryId, HttpServletRequest request) {
+        List<GroupListByCategoryResponse> groupListByCategoryId = groupService.searchGroupsByCategoryId(categoryId);
+        return ResponseEntity.status(200).body(groupListByCategoryId);
+
     }
 
-    //지역별 모임 조회 --로그인 필수
+    //지역별 모임 조회 --예외처리 필요
     @GetMapping(value = "/location/{locationId}")
-    public ResponseEntity<?> searchGroupsByLocationId(@PathVariable Long locationId,  HttpServletRequest request) {
-        String userToken = jwtTokenProvider.resolveToken(request);
-        if(userToken != null) {
-            List<GroupListByLocationResponse> groupListByLocationId = groupService.searchGroupsByLocationId(locationId);
-            return ResponseEntity.status(200).body(groupListByLocationId);
-        }else {
-            throw new UserRuntimeException("모임 지역을 조회하려면 로그인이 필요합니다.");
-        }
+    public ResponseEntity<?> searchGroupsByLocationId(@PathVariable Long locationId, HttpServletRequest request) {
+        List<GroupListByLocationResponse> groupListByLocationId = groupService.searchGroupsByLocationId(locationId);
+        return ResponseEntity.status(200).body(groupListByLocationId);
     }
 
-    //제목으로 모임 조회 -- 로그인 필수
+    //제목으로 모임 조회 --예외처리 필요
     @GetMapping(value = "/title/{title}")
-    public ResponseEntity<?> findByTitleContaining(@PathVariable String title,  HttpServletRequest request) {
-
-        String userToken = jwtTokenProvider.resolveToken(request);
-        if(userToken != null) {
-            List<GroupListByTitleResponse> groupListByTitle = groupService.findByTitleContaining(title);
-            return ResponseEntity.status(200).body(groupListByTitle);
-        }else {
-            throw new UserRuntimeException("모임 지역을 조회하려면 로그인이 필요합니다.");
-        }
+    public ResponseEntity<?> findByTitleContaining(@PathVariable String title, HttpServletRequest request) {
+        List<GroupListByTitleResponse> groupListByTitle = groupService.findByTitleContaining(title);
+        return ResponseEntity.status(200).body(groupListByTitle);
     }
 
 
