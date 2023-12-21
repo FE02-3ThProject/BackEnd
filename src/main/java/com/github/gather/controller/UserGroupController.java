5b1ed30f3f692773b233717b3d49f232935ad65c
ZUA <gimsojeong0228@gmail.com>
2023-12-21 13:26:41 +0900

    기능 설명 수정

--- src/main/java/com/github/gather/controller/UserGroupController.java
+++ src/main/java/com/github/gather/controller/UserGroupController.java
@@ -1,6 +1,5 @@
 package com.github.gather.controller;
 
-import com.github.gather.dto.response.BookMarkResponse;
 import com.github.gather.entity.*;
 import com.github.gather.service.UserGroupService;
 import lombok.RequiredArgsConstructor;
@@ -10,7 +9,6 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 
 @RestController
@@ -26,7 +24,6 @@ public class UserGroupController {
     public ResponseEntity<List<Long>> getJoinedUserGroupIds(@AuthenticationPrincipal User user) {
         if (user == null) {
             // 사용자 정보가 없을 때 처리
-            // 예를 들어 로그인 페이지로 리다이렉트하거나 오류 응답을 고려할 수 있습니다.
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
 
@@ -34,7 +31,7 @@ public class UserGroupController {
         List<Long> joinedGroupIds = userGroupService.getJoinedUserGroupIds(email);
 
         // 디버깅을 위한 로깅
-        System.out.println("가입한 그룹 ID 목록: " + joinedGroupIds);
+        System.out.println("가입한 그룹 모임 목록: " + joinedGroupIds);
 
         return ResponseEntity.ok(joinedGroupIds);
     }
@@ -43,7 +40,6 @@ public class UserGroupController {
     public ResponseEntity<List<Long>> getBookmarkedUserGroupIds(@AuthenticationPrincipal User user) {
         if (user == null) {
             // 사용자 정보가 없을 때 처리
-            // 예를 들어 로그인 페이지로 리다이렉트하거나 오류 응답을 고려할 수 있습니다.
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
 
@@ -51,7 +47,7 @@ public class UserGroupController {
         List<Long> bookmarkedGroupIds = userGroupService.getBookmarkedUserGroupIds(email);
 
         // 디버깅을 위한 로깅
-        System.out.println("즐겨찾기한 그룹 ID 목록: " + bookmarkedGroupIds);
+        System.out.println("즐겨찾기한 모임 ID 목록: " + bookmarkedGroupIds);
 
         return ResponseEntity.ok(bookmarkedGroupIds);
     }
@@ -60,7 +56,7 @@ public class UserGroupController {
     @DeleteMapping("/unbookmark/{groupId}")
     public ResponseEntity<String> unbookmarkUserGroup(@AuthenticationPrincipal User user, @PathVariable Long groupId){
         userGroupService.unbookmarkUserGroup(user, groupId);
-        return ResponseEntity.ok("그룹을 즐겨찾기에서 제거했습니다.");
+        return ResponseEntity.ok("모임을 즐겨찾기에서 제거했습니다.");
     }
 
 }
